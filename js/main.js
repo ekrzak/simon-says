b07264c6acc49d969cc98295744e9a2222a22428
estebankrzak <50186552+estebankrzak@users.noreply.github.com>
2022-01-23 19:05:25 -0300

    Adds js principal features

--- js/main.js
+++ js/main.js
@@ -19,33 +19,77 @@ const colors = {
     3: 'green'
 };
 
-function highlightCells(arr) {
-    
-    arr.forEach(element => { // adds and removes the .active class for elements of the array
+const wait = (delay) => {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, delay);
+    });    
+}
+
+async function highlightCellsComputer(arr) {
+    for (let element of arr) {
+        await wait(750);
         document.querySelector(`#${colors[element]}-cell`).classList.add('active');
-        setTimeout(
-            function() { 
-                document.querySelector(`#${colors[element]}-cell`).classList.remove('active')
-            }, 500);
+        setTimeout(function() {
+            document.querySelector(`#${colors[element]}-cell`).classList.remove('active');
+        }, 200);
+    }
+}
+
+function highlightCellsPlayer(color) {
+    document.querySelector(`#${color}-cell`).classList.add('active');
+    setTimeout(
+        function() { 
+            document.querySelector(`#${color}-cell`).classList.remove('active');
+    }, 150);
+}
+
+function turnMachine(arr, counter) {
+    while (arr.length < counter) {
+        arr.push(generateRandomNumber());
+    }    
+    highlightCellsComputer(arr);
+    console.log(arr)
+}
+
+function turnPlayer() {
+    let array = [];
+
+    document.querySelectorAll('.color-cells').forEach(function (cell) {
+        cell.onclick = function (event) {
+            if (event.target.getAttribute('name') === 'blue') {
+                array.push(0);
+                highlightCellsPlayer(event.target.getAttribute('name'));
+
+            } else if (event.target.getAttribute('name') === 'red') {
+                array.push(1);
+                highlightCellsPlayer(event.target.getAttribute('name'));
+
+            } else if (event.target.getAttribute('name') === 'yellow') {
+                array.push(2);
+                highlightCellsPlayer(event.target.getAttribute('name'));
+
+            } else if (event.target.getAttribute('name') === 'green') {
+                array.push(3);
+                highlightCellsPlayer(event.target.getAttribute('name'));
+            }
+
+            return array;
+
+        };
+
     });
-    
-}
 
-function extendComputerSequence(arr) {
-    arr.push(generateRandomNumber());
-}
-
-function turnMachine(arr) {
-    extendComputerSequence(arr);
-    highlightCells(arr);
 }
 
 function playGame() {
+    let counter = 3;
     let arrayComputer = [];
-    let arrayPlayer = [];
-    
-   turnMachine(arrayComputer);
-   console.log(arrayComputer);
+        
+   turnMachine(arrayComputer, counter);
+   //console.log(arrayComputer);
+    //let arrayPlayer = turnPlayer();
+   
+   
 }
 
 document.querySelector('#play-button').onclick = playGame;
